4fee96268f49ee9d4c64b15c96c2e39c01b3819b
phmoll <philipp.moll@aau.at>
2017-08-23 08:00:33 +0200

    Add L3PacketTracer to Abilene Scenarios

--- scenarios/PIPS-scenario.cpp
+++ scenarios/PIPS-scenario.cpp
@@ -296,6 +296,7 @@ main(int argc, char* argv[])
   pushParticipants.Add(nodeB);
   ns3::ndn::PushTracer::Install(pushParticipants, std::string(logDir + "push-trace.txt"));
   ns3::ndn::L3PacketTracer::InstallAll(std::string(logDir + "packet-trace.txt"));
+  ndn::L3PacketTracer::InstallAll(std::string(logDir + "packet-trace.txt"));
 
 
   Simulator::Stop(MilliSeconds(simTime));
